c358d584e037c4e9701b0be8e6c023627f794e4e
Observability <observability@gmail.com>
2025-04-23 09:18:34 -0300

    update

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
         .then(response => response.json())
         .then(json => {
             data = json;
-            data.sort((a, b) => a.category.localeCompare(b.category));
+            data.sort((a, b) => a.rank - b.rank);
             displayResults(data);
         })
         .catch(error => console.error("Error loading JSON:", error));
@@ -25,11 +25,11 @@ document.addEventListener("DOMContentLoaded", () => {
                 div.classList.add(item.category.toLowerCase());
                 console.log(item.category.toLowerCase());
 
-                div.innerHTML = `<strong>${item.name}</strong>`;  // Exibe o contexto
+                div.innerHTML = `<strong>${item.name}</strong>${item.rank}`;  // Exibe o contexto
 
                 // Adiciona o evento para abrir o modal ao clicar
                 div.addEventListener('click', () => {
-                    openModal(item.name, item.context, item.problem, item.example, item.recommended_solution, item.consequences); // Passando nome, contexto, problema e solução
+                    openModal(item.name, item.category, item.context, item.problem, item.example, item.recommended_solution, item.consequences); // Passando nome, contexto, problema e solução
                 });
 
                 resultsDiv.appendChild(div);
@@ -59,8 +59,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Função para abrir o modal com o conteúdo da caixinha
     // Função para abrir o modal
-    function openModal(name, context, problem, example, recommended_solution, consequences) {
+    function openModal(name, category, context, problem, example, recommended_solution, consequences) {
         document.getElementById("modal-title").textContent = name;
+        document.getElementById("modal-category").innerHTML = `<strong>Category:</strong> ${category}`;
         document.getElementById("modal-context").innerHTML = `<strong>Context:</strong> ${context}`;
         document.getElementById("modal-problem").innerHTML = `<strong>Problem:</strong> ${problem}`;
         document.getElementById("modal-example").innerHTML = `<strong>Example:</strong> ${example}`;
@@ -76,7 +77,7 @@ document.addEventListener("DOMContentLoaded", () => {
             consequencesHTML += "</ul></li>";
         }
         if (consequences.challenges.length > 0) {
-            consequencesHTML += "<li><strong>Challenges:</strong><ul>";
+            consequencesHTML += "<li><strong>Negatives:</strong><ul>";
             consequences.challenges.forEach(item => {
                 consequencesHTML += `<li>${item}</li>`;
             });
@@ -99,7 +100,6 @@ document.addEventListener("DOMContentLoaded", () => {
     // Fecha o modal ao clicar fora dele (na overlay)
     document.getElementById("modal-overlay").addEventListener("click", closeModal);
 
-
     document.querySelectorAll(".close").forEach(closeBtn => {
         closeBtn.addEventListener("click", closeModal);
     });
@@ -110,4 +110,39 @@ document.addEventListener("DOMContentLoaded", () => {
             modal.style.display = "none"; // Oculta o modal
         }
     }
+
+    document.querySelectorAll('.legend-item').forEach(item => {
+        item.addEventListener('click', () => {
+            const selectedCategory = item.getAttribute('data-category').toLowerCase();
+            const filtered = data.filter(obj => obj.category.toLowerCase() === selectedCategory);
+            displayResults(filtered);            
+        });
+    });
+
+    document.querySelectorAll('.relevance-item').forEach(item => {
+        item.addEventListener('click', () => {
+            const selectedCategory = item.getAttribute('data-relevance').toLowerCase();
+            console.log(selectedCategory)
+            var filtered = {};
+            if (selectedCategory === 'high') {
+                filtered = data.filter(obj => obj.rank >= 1 && obj.rank <= 8);                
+            } else if (selectedCategory === 'medium') {
+                filtered = data.filter(obj => obj.rank >= 9 && obj.rank <= 19);                
+            } else {
+                filtered = data.filter(obj => obj.rank >= 20);                
+            }            
+            displayResults(filtered);
+        });
+    });
+
+    document.getElementById('clear-filters').addEventListener('click', () => {
+        document.getElementById('search').value = "";
+        displayResults(data);
+    });
+
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'Escape') {  // Verifica se a tecla pressionada é 'Esc'
+            closeModal();  // Chama a função para fechar o modal
+        }
+    });
 });
